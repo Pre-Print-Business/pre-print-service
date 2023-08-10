162cee6809221d622cf3ca6949846f4e7585ded9
euntaek <choiet4187@naver.com>
2023-08-10 19:56:47 +0900

    feature-update

--- preprint/management/commands/reset_orders.py
+++ preprint/management/commands/reset_orders.py
@@ -1,5 +1,28 @@
+# from django.core.management.base import BaseCommand
+# from django.core.files.storage import default_storage
+# from preprint.models import Order, OrderFile  
+
+# class Command(BaseCommand):
+#     help = 'Reset all orders and files'
+
+#     def handle(self, *args, **options):
+#         orders = Order.objects.all()
+        
+#         for order in orders:
+#             order_files = OrderFile.objects.filter(order=order)
+            
+#             for order_file in order_files:
+#                 if default_storage.exists(order_file.file.name):
+#                     default_storage.delete(order_file.file.name)
+            
+#             order.delete()
+
+#         self.stdout.write(self.style.SUCCESS('Successfully reset all orders and files.'))
+
+
 from django.core.management.base import BaseCommand
 from django.core.files.storage import default_storage
+from django.db import connection
 from preprint.models import Order, OrderFile  
 
 class Command(BaseCommand):
@@ -7,14 +30,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         orders = Order.objects.all()
-        
         for order in orders:
             order_files = OrderFile.objects.filter(order=order)
-            
             for order_file in order_files:
                 if default_storage.exists(order_file.file.name):
                     default_storage.delete(order_file.file.name)
-            
+                order_file.delete()
             order.delete()
 
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM sqlite_sequence WHERE name='preprint_order';")
+            cursor.execute("DELETE FROM sqlite_sequence WHERE name='preprint_orderfile';")
+        
         self.stdout.write(self.style.SUCCESS('Successfully reset all orders and files.'))
